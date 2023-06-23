dd95f87631a5ca828b56f90870933665986d6c80
redsmokelinux <117936863+redsmokelinux@users.noreply.github.com>
2023-06-23 22:59:41 +0530

    Add files via upload

---
+++ hyprland.sh
@@ -0,0 +1,2 @@
+
+sudo dnf install ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel wayland-devel libdrm-devel libxkbcommon-devel systemd-devel libseat-devel mesa-libEGL-devel libinput-devel xcb-util-wm-devel xorg-x11-server-Xwayland-devel mesa-libgbm-devel xcb-util-renderutil-devel
