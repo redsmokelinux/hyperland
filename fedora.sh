1170628f5604c243c67898a37f0a3f4d12878702
redsmokelinux <117936863+redsmokelinux@users.noreply.github.com>
2023-11-22 16:49:12 +0530

    Add files via upload

---
+++ fedora.sh
@@ -0,0 +1,7 @@
+sudo dnf install dnf5
+
+sudo dnf5 install hyprland hyprland-devel 
+
+sudo dnf5 ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel wayland-devel libdrm-devel libxkbcommon-devel systemd-devel libseat-devel mesa-libEGL-devel libinput-devel xcb-util-wm-devel xorg-x11-server-Xwayland-devel mesa-libgbm-devel xcb-util-renderutil-devel ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel rust-xkbcommon-devel libudev-devel libseat-devel mesa-libEGL-devel libinput-devel wayland-devel libdrm-devel libxkbcommon-devel systemd-devel xcb-util-wm-devel xorg-x11-server-Xwayland-devel mesa-libgbm-devel xcb-util-renderutil-devel hwdata-devel libdisplay-info-devel wlroots-devel wayland-devel libdrm-devel libxkbcommon-devel egl-wayland-devel egl-gbm rust-xkbcommon-devel libudev-devel libseat-devel mesa-libEGL-devel libinput-devel libEAI ninja-build cmake meson gcc-c++ libxcb-devel libX11-devel pixman-devel wayland-protocols-devel cairo-devel pango-devel wayland-devel libdrm-devel libxkbcommon-devel systemd-devel libseat-devel mesa-libEGL-devel libinput-devel xcb-util-wm-devel xorg-x11-server-Xwayland-devel mesa-libgbm-devel xcb-util-renderutil-devel  
+
+sudo dnf5 install firefox kitty bemenu nemo gnome-disk-utility
